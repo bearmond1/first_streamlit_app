a6922051cf209ddb76effdb7f3b3fe16938e9c87
bearmond <33288530+bearmond1@users.noreply.github.com>
2024-01-01 12:30:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ except URLError as e:
 #streamlit.stop()
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
 #streamlit.header("The fruit_load_list contains:")
 
 def get_fruit_load_list(my_cnx):
